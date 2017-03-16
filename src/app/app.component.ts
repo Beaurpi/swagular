b38eefca92ec448bec3e9b3ddc8ef5a247598431
Beaurpi <pier.beauregard@gmail.com>
2017-03-16 23:03:59 +0100

    Initial commit
    Put the base of the project and name it :)

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'swg-app',
+  templateUrl: "./app.component.html"
+})
+export class AppComponent  { name = 'Swagular'; }
